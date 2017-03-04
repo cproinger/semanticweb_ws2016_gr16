fe6fe853e2df215722cc2c7466d8edae1906fb45
cproinger <christian.proinger@gmail.com>
2017-03-04 20:57:27 +0100

    courses rdf-turtle

--- src/main/java/at/ac/tuwien/ifs/tulid/group16/SemanticApp.java
+++ src/main/java/at/ac/tuwien/ifs/tulid/group16/SemanticApp.java
@@ -4,23 +4,19 @@ import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
-import org.apache.jena.query.ReadWrite;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SemanticApp {
+	static final String NS_BASE = "http://ifs.tuwien.ac.at/tulid/group16";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SemanticApp.class);
 	}
-
-	static final String NS_BASE = "http://ifs.tuwien.ac.at/tulid/group16";
 
 	@Bean
 	public OntModel ontModel() {
@@ -31,12 +27,12 @@ public class SemanticApp {
 
 	@Bean
 	public Dataset dataset(@Autowired OntModel ontModel) {
-		//longer alternative. 
-//		Dataset ds = DatasetFactory.createTxnMem();
-//		ds.begin(ReadWrite.WRITE);
-//		ds.addNamedModel(SemanticApp.NS_BASE, ontModel);
-//		ds.commit();
-//		return ds;
+		// longer alternative.
+		// Dataset ds = DatasetFactory.createTxnMem();
+		// ds.begin(ReadWrite.WRITE);
+		// ds.addNamedModel(SemanticApp.NS_BASE, ontModel);
+		// ds.commit();
+		// return ds;
 		return DatasetFactory.create(ontModel);
 	}
 }
